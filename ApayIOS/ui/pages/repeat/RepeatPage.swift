bb602d7df9b7c42197bb1ce4b5f860f8c28c0d5f
Mikhail <mikhail.belikov@airbapay.kz>
2023-09-04 17:39:46 +0600

    добавил запрос в повтор

--- ApayIOS/ui/pages/repeat/RepeatPage.swift
+++ ApayIOS/ui/pages/repeat/RepeatPage.swift
@@ -7,6 +7,7 @@ import SwiftUI
 
 struct RepeatPage: View {
     @State var showDialogExit: Bool = false
+    @EnvironmentObject var router: NavigateUtils.Router
 
     var body: some View {
         ZStack {
@@ -48,6 +49,25 @@ struct RepeatPage: View {
                                 })
                 )
                 .onTapGesture(perform: { showDialogExit = false })
+                .onAppear {
+                    Task {
+                        if let response = await paymentAccountEntryRetryService() {
+                            if response.isSecure3D == true {
+                                router.route(to: \.webViewPage, response.secure3D?.action)
+
+                            } else if (response.errorCode != "0") {
+                                router.route(to: \.errorPage, Int(response.errorCode ?? "1"))
+
+                            } else {
+                                router.route(to: \.successPage)
+                            }
+
+                        } else {
+                            router.route(to: \.errorPage, 1)
+
+                        }
+                    }
+                }
     }
 
 }
@@ -59,107 +79,3 @@ struct RepeatPage_Previews: PreviewProvider {
     }
 }
 
-/* fun RepeatPage(
-    navController: NavController,
-    paymentsRepository: PaymentsRepository
-) {
-
-    BackHandler {}
-
-    ConstraintLayout(
-        modifier = Modifier
-            .background(ColorsSdk.bgMain)
-            .clipToBounds()
-            .fillMaxSize()
-    ) {
-
-        val (spaceRef, progressRef, textRef, text2Ref) = createRefs()
-
-        Spacer(
-            modifier = Modifier
-                .fillMaxHeight(0.35f)
-                .constrainAs(spaceRef) {
-                    top.linkTo(parent.top)
-                }
-        )
-
-        Text(
-            text = weRepeatYourPayment(),
-            style = LocalFonts.current.h3,
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .constrainAs(textRef) {
-                    top.linkTo(spaceRef.bottom)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                }
-        )
-
-        Text(
-            text = thisNeedSomeTime(),
-            style = LocalFonts.current.regular,
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .padding(top = 16.dp)
-                .constrainAs(text2Ref) {
-                    top.linkTo(textRef.bottom)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                }
-        )
-
-        CircularProgressIndicator(
-            color = ColorsSdk.colorBrandMainMS.value,
-            modifier = Modifier
-                .padding(top = 24.dp)
-                .fillMaxWidth(0.3f)
-                .constrainAs(progressRef) {
-                    top.linkTo(text2Ref.bottom)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                }
-        )
-    }
-
-    LaunchedEffect("onStart") {
-        launch {
-            onStart(
-                navController = navController,
-                paymentsRepository = paymentsRepository
-            )
-        }
-    }
-}
-
-private fun onStart(
-    navController: NavController,
-    paymentsRepository: PaymentsRepository
-) {
-
-    paymentsRepository.paymentAccountEntryRetry(
-        result = { response ->
-            if (response.isSecure3D == true) {
-                openWebView(
-                    secure3D = response.secure3D,
-                    navController = navController
-                )
-
-            } else if (response.errorCode != "0") {
-                openErrorPageWithCondition(
-                    errorCode = response.errorCode?.toInt() ?: 0,
-                    navController = navController
-                )
-
-            } else {
-                openSuccess(navController)
-            }
-        },
-        error = {
-            openErrorPageWithCondition(
-                errorCode = ErrorsCode.error_1.code,
-                navController = navController
-            )
-        }
-    )
-
-}*/
