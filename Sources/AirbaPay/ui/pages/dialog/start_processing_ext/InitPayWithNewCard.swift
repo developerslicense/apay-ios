26cb71130c7379f70139d7b0e6bd5f0fafedd759
Mikhail <mikhail.belikov@airbapay.kz>
2023-12-12 11:39:29 +0600

    правки по верстке

--- Sources/AirbaPay/ui/pages/dialog/start_processing_ext/InitPayWithNewCard.swift
+++ Sources/AirbaPay/ui/pages/dialog/start_processing_ext/InitPayWithNewCard.swift
@@ -9,39 +9,43 @@ struct InitViewStartProcessingPayWithNewCard: View {
     var actionClick: () -> Void
 
     var body: some View {
-        HStack {
-            Spacer()
+        Button(
+                action: { actionClick() },
+                label: {
+                    HStack {
+                        Spacer()
 
-            ZStack {
-                Rectangle()
-                        .fill(ColorsSdk.colorBrandMain)
-                        .frame(height: 1.5)
-                        .frame(width: 8)
+                        ZStack {
+                            Rectangle()
+                                    .fill(ColorsSdk.colorBrandMain)
+                                    .frame(height: 1.5)
+                                    .frame(width: 8)
 
-                Rectangle()
-                        .fill(ColorsSdk.colorBrandMain)
-                        .frame(height: 8)
-                        .frame(width: 1.5)
-            }
+                            Rectangle()
+                                    .fill(ColorsSdk.colorBrandMain)
+                                    .frame(height: 8)
+                                    .frame(width: 1.5)
+                        }
 
 
-            Text(payAnotherCard())
-                    .foregroundColor(ColorsSdk.colorBrandMain)
-                    .textStyleSemiBold()
-                    .padding(.leading, 6)
-                    .padding(.top, 7)
-                    .padding(.bottom, 7)
+                        Text(payAnotherCard())
+                                .foregroundColor(ColorsSdk.colorBrandMain)
+                                .textStyleSemiBold()
+                                .padding(.leading, 6)
+                                .padding(.top, 7)
+                                .padding(.bottom, 7)
 
-            Spacer()
-        }
+                        Spacer()
+                    }
 
-                .overlay(
-                        RoundedRectangle(cornerRadius: 8)
-                                .stroke(ColorsSdk.gray10, lineWidth: 1)
-                )
-                .frame(width: .infinity)
-                .padding(.horizontal, 16)
-                .onTapGesture(perform: { actionClick() })
+                            .overlay(
+                                    RoundedRectangle(cornerRadius: 8)
+                                            .stroke(ColorsSdk.gray10, lineWidth: 1)
+                            )
+                            .frame(width: .infinity)
+                            .padding(.horizontal, 16)
 
+                }
+        )
     }
 }
