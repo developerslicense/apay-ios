34a3c947b97a77020f2c71b8a14ad060ff0ff5a6
Mikhail <mikhail.belikov@airbapay.kz>
2024-02-28 14:50:03 +0600

    правка бага

--- Sources/AirbaPay/network/base/NetworkModules.swift
+++ Sources/AirbaPay/network/base/NetworkModules.swift
@@ -82,7 +82,7 @@ actor NetworkManager: GlobalActor {
 
                             headers: [
                                 "Content-Type": "application/json; charset=utf-8",
-                                "Platform": "iOS, 1.0.56",
+                                "Platform": "iOS, 1.0.52",
                                 "Authorization":
                                 DataHolder.accessToken == nil
                                         || DataHolder.accessToken == "" ? "" : "Bearer " + DataHolder.accessToken!
