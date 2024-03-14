33d3a9e6f8b313d38425d6806fa8a34e3d513650
Mikhail <mikhail.belikov@airbapay.kz>
2024-03-14 14:33:56 +0500

    доработки по эпплпэй

--- Sources/AirbaPay/data/utils/DataHolder.swift
+++ Sources/AirbaPay/data/utils/DataHolder.swift
@@ -5,7 +5,7 @@
 import Foundation
 
 class DataHolder {
-    static var sdkVersion = "1.0.59"
+    static var sdkVersion = "1.0.60"
     static var baseUrl = ""
 
     static var connectTimeout = 60
