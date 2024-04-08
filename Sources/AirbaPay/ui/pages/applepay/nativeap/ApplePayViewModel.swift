0aff7f92f28ab4213bec3e48306eb1e42ead6d4d
Mikhail <mikhail.belikov@airbapay.kz>
2024-04-08 17:26:53 +0500

    правки по эпплпэй

--- Sources/AirbaPay/ui/pages/applepay/nativeap/ApplePayViewModel.swift
+++ Sources/AirbaPay/ui/pages/applepay/nativeap/ApplePayViewModel.swift
@@ -7,68 +7,97 @@
 import Foundation
 
 public class ApplePayViewModel {
-    
+
     public func auth(
-        onError: @escaping () -> Void,
-        onSuccess: @escaping () -> Void
+            onError: @escaping () -> Void,
+            onSuccess: @escaping () -> Void
     ) {
-           let params = AuthRequest(
+        let params = AuthRequest(
                 password: DataHolder.password,
                 paymentId: nil,
                 terminalId: DataHolder.terminalId,
                 user: DataHolder.shopId
-           )
+        )
 
-           Task {
-               if let resultAuth = await authService(params: params) {
-                   DataHolder.accessToken = resultAuth.accessToken
-                   
-                   if let resultCreatePayment = await createPaymentService() {
-                       let params = AuthRequest(
-                           password: DataHolder.password,
-                           paymentId: resultCreatePayment.id,
-                           terminalId: DataHolder.terminalId,
-                           user: DataHolder.shopId
-                       )
-                       
-                       if let result = await authService(params: params) {
-                            onSuccess()
-                       } else {
-                           onError()
-                       }
-                       
-                   } else {
-                       onError()
-                   }
-               } else {
-                   onError()
-               }
-           }
-       }
+        Task {
+            if let resultAuth = await authService(params: params) {
+                DataHolder.accessToken = resultAuth.accessToken
 
-       public func processingWallet(
+                if let resultCreatePayment = await createPaymentService() {
+                    let params = AuthRequest(
+                            password: DataHolder.password,
+                            paymentId: resultCreatePayment.id,
+                            terminalId: DataHolder.terminalId,
+                            user: DataHolder.shopId
+                    )
+
+                    if let result = await authService(params: params) {
+                        onSuccess()
+                    } else {
+                        onError()
+                    }
+
+                } else {
+                    onError()
+                }
+            } else {
+                onError()
+            }
+        }
+    }
+
+    public func processingWallet(
             navigateCoordinator: AirbaPayCoordinator,
             applePayToken: String
-       ) {
+    ) {
 
-           Task {
-               if let result = await putPaymentWallet(applePayToken: applePayToken) {
-                   if result.errorCode != 0 {
-                       let error = ErrorsCode(code: result.errorCode ?? 1).getError()
-                       navigateCoordinator.openErrorPageWithCondition(errorCode: error.code)
+        Task {
+            if let result = await putPaymentWallet(applePayToken: applePayToken) {
+                if result.errorCode != 0 {
+                    let error = ErrorsCode(code: result.errorCode ?? 1).getError()
+                    DataHolder.externalApplePayRedirect = (nil, false)
+                    redirectTo(
+                            defaultRedirectAction: {
+                                navigateCoordinator.openErrorPageWithCondition(errorCode: error.code)
+                            }
+                    )
 
-                   } else if result.isSecure3D == true {
-                       navigateCoordinator.openAcquiring(
-                            redirectUrl: result.secure3D?.action
-                       )
+                } else if result.isSecure3D == true {
+                    let url = result.secure3D?.action
+                    DataHolder.externalApplePayRedirect = (url, false)
+                    redirectTo(
+                            defaultRedirectAction: {
+                                navigateCoordinator.openAcquiring(redirectUrl: url)
+                            }
+                    )
 
-                   } else {
-                       navigateCoordinator.openSuccess()
-                   }
-                   
-               } else {
-                   navigateCoordinator.openErrorPageWithCondition(errorCode: ErrorsCode().error_1.code)
-               }
-           }
-       }
+                } else {
+                    DataHolder.externalApplePayRedirect = (nil, true)
+                    redirectTo(
+                            defaultRedirectAction: {
+                                navigateCoordinator.openSuccess()
+                            }
+                    )
+                }
+
+            } else {
+                DataHolder.externalApplePayRedirect = (nil, false)
+                redirectTo(
+                        defaultRedirectAction: {
+                            navigateCoordinator.openErrorPageWithCondition(errorCode: ErrorsCode().error_1.code)
+                        }
+                )
+            }
+        }
+    }
+
+    func redirectTo(
+            defaultRedirectAction: () -> Void
+    ) {
+        if DataHolder.redirectFromStoryboardToSwiftUi != nil {
+            DataHolder.redirectFromStoryboardToSwiftUi!()
+        } else {
+            defaultRedirectAction()
+        }
+    }
 }
