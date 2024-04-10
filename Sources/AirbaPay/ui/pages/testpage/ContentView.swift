2b25f80a509b62b9f5be7e01ac88a934968408b0
Mikhail <mikhail.belikov@airbapay.kz>
2024-04-10 17:16:54 +0500

    правки

--- Sources/AirbaPay/ui/pages/testpage/ContentView.swift
+++ Sources/AirbaPay/ui/pages/testpage/ContentView.swift
@@ -19,6 +19,8 @@ struct TestPage1: View {
     )
 
     var body: some View {
+        let applePay = ApplePayManager(navigateCoordinator: navigateCoordinator)
+
         AirbaPayView(
                 navigateCoordinator: navigateCoordinator,
                 contentView: {
@@ -70,6 +72,8 @@ struct TestPage1: View {
                                     action: {
                                         testInitSdk(autoCharge: autoCharge  ? 1 : 0)
                                         navigateCoordinator.openTestApplePaySwiftUi()
+
+//                                      applePay.buyBtnTapped()
                                     },
                                     label: {
                                         Text("Тест внешнего applePay механизма")
