d150d50c6bc5fd758813b3673a299251bd8e7004
Mikhail <mikhail.belikov@airbapay.kz>
2024-05-18 21:41:28 +0500

    мелкая правка

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         dependencies: [
             .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.8.0")),
             .package(url: "https://github.com/sanzaru/SimpleToast.git", from: "0.0.1"),
-            .package(url: "https://github.com/gaetanomatonti/BottomSheet", .upToNextMajor(from: "5.2.0"))
+            .package(url: "https://github.com/finn-no/BottomSheet.git", .upToNextMajor(from: "5.2.0"))
         ],
         targets: [
             // Targets are the basic building blocks of a package, defining a module or a test suite.
