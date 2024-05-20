de6acaab502ab9ae5bf2012244a309668459902f
Mikhail <mikhail.belikov@airbapay.kz>
2024-05-20 11:53:01 +0500

    мелкая правка

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         dependencies: [
             .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.8.0")),
             .package(url: "https://github.com/sanzaru/SimpleToast.git", from: "0.0.1"),
-            .package(url: "https://github.com/finn-no/BottomSheet.git", .upToNextMajor(from: "5.2.0"))
+            .package(name: "FINNBottomSheet", url: "https://github.com/finn-no/BottomSheet.git", .upToNextMajor(from: "1.0.0"))
         ],
         targets: [
             // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -26,7 +26,7 @@ let package = Package(
             .target(
                     name: "AirbaPay",
                     dependencies: [
-                        "Alamofire", "SimpleToast", "BottomSheet"
+                        "Alamofire", "SimpleToast", "FINNBottomSheet"
                     ],
                     resources: [
                         .process("resources"),
